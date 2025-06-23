5a9c0d21c4e84c2e0de9b6d4e32a79a3247d3729
Andy Lu <andy.lu@qlik.com>
2025-06-23 16:38:39 -0400

    Bump backoff to a newer version (#39)
    * Bump backoff to a newer version
    
    * Bump singer-python so that backoff versions match
    
    * Bump version, update changelog (#41)
    
    * Update setup.py
    
    * Update changelog

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@
 from setuptools import setup, find_packages
 
 setup(name='tap-impact',
-      version='2.1.1',
+      version='2.1.2',
       description='Singer.io tap for extracting data from the Impact Advertiser, Partner, Agency APIs',
       author='jeff.huth@bytecode.io',
       classifiers=['Programming Language :: Python :: 3 :: Only'],
       py_modules=['tap_impact'],
       install_requires=[
-          'backoff==1.8.0',
+          'backoff==1.10.0',
           'requests==2.32.4',
-          'singer-python==5.8.1'
+          'singer-python==5.13.2'
       ],
       extras_require={
           'dev': [
